1f2cdd85a68abacb80dd72f6f2c889d370e9a954
mistermboy <pabloyo97@hotmail.com>
2020-04-28 13:57:15 +0100

    updateTable

--- src/main.js
+++ src/main.js
@@ -7,9 +7,6 @@ yate.setSize(null,550);
 yashe.setSize(null,550);
 yasme.setSize(null,100);
 
-
-mdc.ripple.MDCRipple.attachTo(document.querySelector('.foo-button'));
-
 function sendToJava () {
     updateTable();
     javaConnector.toLowerCase(yashe.getValue());
@@ -41,4 +38,65 @@ function updateTable(){
                 '<tbody id="tBody"/>'+
                 '</table>'+
             '<div>'));
+
+            let obj = 
+
+             [
+                 {
+                     node:'<jhon>',
+                     shape:'<Person>',
+                     details:'conformant'
+                 },
+                 {
+                     node:'<Pluto>',
+                     shape:'<Person>',
+                     details:'nonconformant'
+                 }
+             ];
+            
+
+            for(let i in obj){
+                console.log(obj[i])
+                let succces = 'table-success';
+                let id = $('<td>').text("0");
+                let node = $('<td>').text(obj[i].node);
+                let shape = $('<td>').text(obj[i].shape)
+                let details = $('<td>').text(obj[i].details);
+                
+                if(obj[i].details == 'nonconformant'){
+                    succces = 'table-danger';
+                }
+
+                console.log(succces)
+
+                $('#tBody')
+                    .append(
+                      $('<tr class='+succces+'>')
+                      .append(id)
+                      .append(node)
+                      .append(shape)
+                      .append(details)
+                    ) 
+        
+            }
+               /*  let id = $('<td>').text("0");
+                let node = $('<td>').append());
+                let shape = showQualify(el.shape,data.shapesPrefixMap);
+                let details = $('<td>').append($('<details><pre>').text(el.reason));
+                if(typeof shape == 'object'){
+                shape = $('<td>').append(shape);
+                }else{
+                shape = $('<td>').text(shape);
+                }
+                
+
+                  $('#tBody')
+                    .append(
+                      $('<tr class='+succces+'>')
+                      .append(id)
+                      .append(node)
+                      .append(shape)
+                      .append(details)
+                    ) 
+                }) */
 }
